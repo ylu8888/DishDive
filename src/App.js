34e8bb78739b42d7433c18db27263ecba2bd430e
ylu8888 <yang.lu.3@stonybrook.edu>
2023-07-06 19:30:00 -0400

    added comments

--- src/App.js
+++ src/App.js
@@ -9,34 +9,39 @@ const App = () => {
   const APP_ID = "462cfed0";
   const APP_KEY = "37538db0ba40a08befe71d08c7b476f6";
 
-  const [recipes, setRecipes] = useState([]);
-  const [search, setSearch] = useState("");
-  const [query, setQuery] = useState("Vanilla Sponge Cake");
+  //states are similar to variables and useState declares/initializes state variables. "setState" are the setters that update variables
+  const [recipes, setRecipes] = useState([]); //setRecipes(data.hits) stores the API data to 'recipes' state
+  const [search, setSearch] = useState("");  //state to store user input from search bar, initially empty
+  const [query, setQuery] = useState("Vanilla Sponge Cake");  //state to store the final input when onSearch
 
-  useEffect( () =>{
+  useEffect( () =>{ //activates on page load, useEffect hook is used to fetch recipes from API when query state changes
     getRecipes();   
-  }, [query]);
+  }, [query]); 
 
-  const getRecipes = async () => {
-    const response = await fetch(
-      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+  //async operations are important for handling tasks that take time to complete such as fetching API Data, it allows other parts of code to 
+  //continue executing while async task is being processed, this reduces overall processing time and maintains a responsive user experience
+  // 'async' keyword marks function as asynchronous, allowing use of 'await' keyword, which pauses execution of function until promise is resolved
+  const getRecipes = async () => { //'await' promise in this case: fetching response from API, NOTE*: doesn't pause the entire JS code, only this function
+    const response = await fetch( //fetches recipes from API based on query state. Fetch sends a GET request to API endpoint 
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}` //query, APP_ID, APP_KEY are parameters of GET request
     );
-    const data = await response.json();
-    setRecipes(data.hits); 
+    const data = await response.json();  //response is converted to JSON format 
+    setRecipes(data.hits); //Retried recipe data is stored in recipes
     console.log(data.hits);
   };
 
-  const getSearch = e =>{
-    e.preventDefault();
-    setQuery(search);
-    setSearch(''); 
-  }
-
+  //updates the search with onChange event, which stores value of the user's input field
   const updateSearch = e => {
     setSearch(e.target.value);
     console.log(search)
   };
 
+  //function triggers when user clicks on search, which updates query
+  const getSearch = e =>{
+    e.preventDefault(); //prevents default form submission which causes page to reload
+    setQuery(search);
+    setSearch(''); 
+  }
 
   return (
     <div className="App">
@@ -49,9 +54,10 @@ const App = () => {
 
       </form>
       <div className="recipes">
-      {recipes.map(recipe =>(
+      {recipes.map(recipe =>( //maps/iterates over the recipe array
         <Recipe 
-        key={recipe.recipe.label}
+        //passes the state data as prop to render actual item values for this component
+        key={recipe.recipe.label} //key prop for proper rendering and prevent browser compile error
         title={recipe.recipe.label} 
         calories={recipe.recipe.calories} 
         image={recipe.recipe.image}
@@ -62,7 +68,6 @@ const App = () => {
       <Footer />
     </div>
    
-
   );
 }
 
