006c573860ac7d33f01ef7f571929b1999117a1f
ylu8888 <yang.lu.3@stonybrook.edu>
2023-07-05 22:31:40 -0400

    added title

---
+++ src/Title.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Title = () => {
+    return(
+        <div className="maintitle">
+        <h1 className="recipesearch">🍕 RECIPE SEARCH 🍔</h1>
+          <h3 className="readme">DishDive has the recipes for tens of thousands of dishes from all over the world. Simply enter a food and get cooking!</h3>
+          
+
+      </div>
+    );
+}
+
+export default Title;
