ab032a160e5f058278766cb9dffe5ad5f375b9e2
ylu8888 <yang.lu.3@stonybrook.edu>
2023-07-05 23:25:23 -0400

    changed bg colors

--- src/Title.js
+++ src/Title.js
@@ -7,7 +7,7 @@ const Title = () => {
         <h2 className={style.header}> DishDive</h2>
             <div>
             <h1 className={style.recipesearch}>🍕 RECIPE SEARCH 🍔</h1>
-            <h3 className={style.readme}>DishDive has the recipes for tens of thousands of dishes from all over the world. Simply enter a food and get cooking!</h3>
+            <h3 className={style.readme}>DishDive has the recipes for millions of dishes from all over the world. Simply enter a food and get cooking!</h3>
             </div>
 
         </div>
