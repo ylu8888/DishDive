bce819d6955a6e33d0b54d20899ca789abd3ec9c
ylu8888 <yang.lu.3@stonybrook.edu>
2023-07-06 20:42:51 -0400

    added comments

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import style from './recipe.module.css';
 
+// The App components state data gets passed to Recipe component, which takes in set of input called props
+//Recipe component takes in an object with properties "title, calories, img, ingred" as its props
 const Recipe = ({title, calories, image, ingred}) => {
     return (
     <div className={style.recipe}>
     
         <h1 className={style.title}>{title}</h1>
         <ul className={style.ingred}>
-            {ingred.map(ingred =>(
+            {ingred.map(ingred =>( //mapping over ingredients which is an array 
                 <li className={style.listitem}>{ingred.text}</li>
             ))}
         </ul>
